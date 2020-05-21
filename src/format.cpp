52055c4a8ce2960bd3da281b9e7434b53638025f
gepizar <gepizarro@uc.cl>
2020-05-21 14:44:26 -0400

    feat: ElapsedTime
    Change from measuring seconds to format HH:MM:SS

--- src/format.cpp
+++ src/format.cpp
@@ -8,4 +8,28 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) { 
+    string HH, MM, SS; 
+    int H, M, S; 
+    H = seconds / 3600; 
+    M = (seconds % 3600) / 60; 
+    S = seconds % 60; 
+
+    HH = std::to_string(H); 
+    MM = std::to_string(M); 
+    SS = std::to_string(S); 
+
+    if (S < 10) {
+        SS = "0" + SS; 
+    }
+
+    if (M < 10) {
+        MM = "0" + MM; 
+    }
+
+    if (H < 10) {
+        HH = "0" + HH; 
+    }
+
+    return HH + ":" + MM + ":" + SS; 
+}
