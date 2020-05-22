85f5a92852c074349bdc85c52458ea91ced29902
gepizar <gepizarro@uc.cl>
2020-05-22 13:39:57 -0400

    feat: PID ram
    Function implemented and working.

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -176,7 +176,20 @@ string LinuxParser::Command(int pid) {
 
 // TODO: Read and return the memory used by a process
 // REMOVE: [[maybe_unused]] once you define the function
-string LinuxParser::Ram(int pid[[maybe_unused]]) { return string(); }
+string LinuxParser::Ram(int pid) { 
+  string line, key, value; 
+  std::ifstream stream(kProcDirectory + to_string(pid) + kStatusFilename);
+  if (stream.is_open()) {
+    while (std::getline(stream, line)) {
+      std::istringstream linestream(line); 
+      while (linestream >> key >> value) {
+        if (key == "VmSize:") {
+          return value; 
+        }
+      }
+    }
+  }
+  return string(); }
 
 // TODO: Read and return the user ID associated with a process
 // REMOVE: [[maybe_unused]] once you define the function
--- src/process.cpp
+++ src/process.cpp
@@ -13,6 +13,7 @@ using std::vector;
 
 Process::Process(int pid) : pid_(pid) {
     command_ = LinuxParser::Command(pid_);
+    ram_ = LinuxParser::Ram(pid_); 
 }
 // TODO: Return this process's ID
 int Process::Pid() { return pid_; }
@@ -24,7 +25,7 @@ float Process::CpuUtilization() { return 0; }
 string Process::Command() { return command_; }
 
 // TODO: Return this process's memory utilization
-string Process::Ram() { return string(); }
+string Process::Ram() { return ram_; }
 
 // TODO: Return the user (name) that generated this process
 string Process::User() { return string(); }
