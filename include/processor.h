611357f3441b1f4b735c237e5abd235bd7caffa1
gepizar <gepizarro@uc.cl>
2020-05-22 19:13:15 -0400

    feat: Processor Class
    Add a Constructor and complete the Utilization funct. Delta of util.

--- include/processor.h
+++ include/processor.h
@@ -7,6 +7,8 @@ class Processor {
 
   // TODO: Declare any necessary private members
  private:
+  long total_{0}; 
+  long idle_{0}; 
 };
 
 #endif
--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,19 @@
 #include "processor.h"
+#include "linux_parser.h"
+
+#include <iostream>
 
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization() { 
+
+    long total =  LinuxParser::Jiffies();
+    long idle = LinuxParser::IdleJiffies(); 
+    
+    long prevIdle = idle_;
+    long prevTotal  = total_; 
+
+    total_ = total; 
+    idle_ = idle; 
+
+    return  1.0 - (float)(idle - prevIdle) / (total - prevTotal);  
+}
